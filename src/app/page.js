8ca616b57f94b0a421835f358df731d12b022d53
Neel Bhavsar <neelbhavsarr1@gmail.com>
2026-02-07 16:47:40 +0000

    Working toggle component, next add homenavbar + global css for toggle themes

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import ToggleButton from "@/components/ToggleButton/ToggleButton";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>Welcome to LocalHands!</h1>
+      <ToggleButton />
     </div>   
   );
 }
--- src/components/ToggleButton/ToggleButton.js
+++ src/components/ToggleButton/ToggleButton.js
@@ -1,9 +1,28 @@
+"use client"
 import styles from "./ToggleButton.module.css";
+import Image from "next/image";
+import { useEffect } from "react";
+import { useState } from "react";
+
+/*
+this is the toggle button component, it will be used to toggle between light and dark mode
+it will be used in the navbar components for both homeNavBar and loggedinNavBar, it will be 
+used to toggle between light and dark mode for the entire website.
+*/
 
 export default function ToggleButton() {
-    return (
-        <div>
+    const [isToggled, setIsToggled] = useState(false);
 
+    const toggleButton = () => {
+        setIsToggled(!isToggled);
+        console.log("Toggled", !isToggled);
+    };
+
+    return (
+        <div className={styles.toggleButtonContainer}>
+            <button type="button" className={styles.toggleButton} onClick={toggleButton}>
+                <Image src={isToggled ? "/moon.png" : "/sun.png"} alt="blooper!!" width={24} height={24} />
+            </button>
         </div>
     );
 }
