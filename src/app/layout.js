e0d7903be1f3cc98c464837b97a88d37648ab657
Neel Bhavsar <neelbhavsarr1@gmail.com>
2026-02-07 15:57:56 +0000

    Setup basics for themeProvider for toggle selection + basic css for application

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Providers } from "./providers";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "LocalHands",
@@ -18,9 +8,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- src/app/providers.js
+++ src/app/providers.js
@@ -1,5 +1,10 @@
 "use client"
 
+/**
+ * this component is used to wrap the entire application with the themeprovider from next-themes.
+ * it allows us to easily switch between light and dark themes based on the user's system preferences.
+ */
+
 import { ThemeProvider } from "next-themes"
 
 export function Providers({ children }) {
