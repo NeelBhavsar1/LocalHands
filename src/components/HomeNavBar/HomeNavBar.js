1d61ff1298caebdcc06827852b1a6811f2d2ac59
Neel Bhavsar <neelbhavsarr1@gmail.com>
2026-02-12 10:41:30 +0000

    v2 logo + mobile drop down fixed

--- src/components/HomeNavBar/HomeNavBar.js
+++ src/components/HomeNavBar/HomeNavBar.js
@@ -4,6 +4,15 @@ import styles from "./HomeNavBar.module.css";
 import Image from "next/image";
 import { useState } from "react";
 
+
+/*
+Features to implement:
+1) Language selection dropdown using next-i18next
+2) Fix mobile hamburger menu dropdown
+3) Update the CSS for the mobile menu to ensure it displays correctly on all screen sizes
+4) Provide global CSS for the toggle buttons!
+*/
+
 export default function HomeNavBar() {
     
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -16,7 +25,8 @@ export default function HomeNavBar() {
     return (
         <div className={styles.container}>
             <div className={styles.logo}>
-                <Image src="/logo.png" alt="LocalHands Logo" width={100} height={100} className={styles.localHandsImage}/>
+                <Image src="/logo-v2.png" alt="LocalHands Logo" width={100} height={100} className={styles.localHandsImage}/>
+                <p>Local<span>Hands</span></p>
             </div>
 
             <div className={styles.navLinks}>
@@ -25,7 +35,7 @@ export default function HomeNavBar() {
                 <a href="#about">About Us</a>
                 <a href="#contact">Contact</a>
             </div>
-
+            
             <div className={styles.userActions}>
                 <ToggleButton />
                 <button className={styles.loginButton}>Login</button>
@@ -44,10 +54,12 @@ export default function HomeNavBar() {
                     <a href="#home" onClick={toggleMobileMenu}>Home</a>
                     <a href="#services" onClick={toggleMobileMenu}>Services</a>
                     <a href="#about" onClick={toggleMobileMenu}>About Us</a>
+                    <a href="#contact" onClick={toggleMobileMenu}>Contact</a>
+
 
                     <div className={styles.actionButton}>
+                        <ToggleButton />
                         <button className={styles.loginButton}>Login</button>
-                        <button>Sign Up</button>
                     </div>
                 </div>
             }
