05709757bfe1344944ac1f23cdd05f82c2caac89
Neel Bhavsar <neelbhavsarr1@gmail.com>
2026-03-04 11:25:27 +0000

    designed the footer component + mobile responsiveness + no implementation of links yet

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import ToggleButton from "@/components/ToggleButton/ToggleButton";
 import HomeNavBar from "@/components/HomeNavBar/HomeNavBar";
 import styles from "./page.module.css";
 import LanguageButton from "@/components/LanguageButton/LanguageButton";
+import Footer from "@/components/Footer/Footer";
 
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <HomeNavBar />
-      
+      <Footer />
     </div>   
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,31 @@
 "use client";
 
+import Link from 'next/link';
 import styles from './Footer.module.css'
 import Image from 'next/image'
 
 export default function Footer() {
   return (
-    <div className={styles.container}>
-        <div className={styles.leftColumn}>
+    <div className={styles.wrapper}>
 
-        </div>
-        <div className={styles.middleColumns}>
-        
-
-        </div>
-        <div className={styles.rightColumn}>
-
-        </div>
-        
+      <div className={styles.container}>
+          <div className={styles.leftColumn}>
+            <Image src="/logo-v2.png" alt="LocalHands Logo" width={100} height={100} className={styles.localHandsImage}/>
+            <p>Local<span>Hands</span></p>
+          </div>
+          <div className={styles.middleColumns}>
+            <h2>Links</h2>
+            <div className={styles.linkColumn}>
+              <Link href='/'>Home</Link>
+              <Link href='/'>FAQ</Link>
+              <Link href='/'>Contact Us</Link>
+              <Link href='/'>Terms of Service</Link>
+              <Link href='/'>Privacy Policy</Link>
+              <Link href='/'>Site Map</Link>
+            </div>
+          </div>           
+      </div>
+      <p className={styles.copyWriteLolHailCC}>© {new Date().getFullYear()} LocalHands</p>
     </div>
   )
 }
