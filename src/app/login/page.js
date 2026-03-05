4a25cde7373911d22d6460798abd5d8d97b28c85
Neel Bhavsar <neelbhavsarr1@gmail.com>
2026-03-05 16:16:17 +0000

    updated login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,29 +1,38 @@
-"use client";
+"use client"
 
-import React from "react";
-import styles from "./page.module.css";
-import HomeNavBar from "@/components/HomeNavBar/HomeNavBar";
+import React from 'react'
+import styles from './page.module.css'
+import HomeNavBar from '@/components/HomeNavBar/HomeNavBar'
+import Link from 'next/link'
 
-export default function Page() {
+export default function page() {
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    const submitForm = () => {
+        console.log("Form submitted!");
+    }
 
   return (
-      <div className={styles.container}>
-        <div className={styles.card}>
-        <h1>Welcome Back</h1> 
-        <form className={styles.formContainer} onSubmit={handleSubmit}>
-          <label>Email <input type="email" name="email" required placeholder="john@example.com"/></label>
+    <div className={styles.wrapper}>
+        <HomeNavBar />
+        <h1>LocalHands</h1>
 
-          <label>Password <input type="password" name="password" required placeholder="Enter your password"/></label>
+        <div className={styles.container}>  
+            
+            <form name='login-form' required onChange={submitForm} className={styles.loginForm}>
+                <label htmlFor='email'>Email 
+                    <input type='email' id='email' name='email' required placeholder='jDoe@gmail.com' />
+                </label>
 
-          <button type="submit">Sign in</button>
-        </form>
+                <label htmlFor='password'>Password
+                    <input type='password' id='pasword' name='password' required />
+                </label>
+                <p className={styles.goToSignUp}>Don't have an account? <span><Link href="/">Click here</Link></span></p>
+                <input type='submit' value='Continue' className={styles.submitForm} />
 
-        <p>Don’t have an account?{" "} <span className={styles.reRoute}>Sign up</span></p>
+            </form>
+
+
         </div>
-      </div>
-  );
+    </div>
+  )
 }
