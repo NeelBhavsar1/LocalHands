d1b587e2550dc6bd73c1ed7099e72eb242b3553a
Rocster47 <rocster2130@gmail.com>
2026-03-09 15:27:39 +0000

    Add backend project directory and put frontend in new frontend subdirectory.

---
+++ localhands-backend/src/main/java/com/localhands/backend/LocalhandsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.localhands.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalhandsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalhandsBackendApplication.class, args);
+	}
+
+}
