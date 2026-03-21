3a57ee2bf13f06983b846afd4ae41211ecbc4b97
Neel Bhavsar <neelbhavsarr1@gmail.com>
2026-03-21 17:57:36 +0000

    updates frontend form logic on login page

--- localhands-frontend/src/app/login/page.js
+++ localhands-frontend/src/app/login/page.js
@@ -1,14 +1,56 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./page.module.css";
 import HomeNavBar from "@/components/HomeNavBar/HomeNavBar";
 import Link from "next/link";
 
 export default function page() {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+
+    setFormData((prev) => ({...prev, [name]: value}))
+  }
+
+  const validateForm = () => {
+
+    const newErorrs = {};
+
+    if (!formData.email.trim()) {
+      newErorrs.email = "Email is required!"
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErorrs.email = "Enter a valid email!";
+    }
+
+    if (!formData.password) {
+      newErorrs.password = "Password is required!"
+    } else if (formData.password.length < 8) {
+      newErorrs.password = "Password must be at least 8 characters"
+    }
+
+    return newErorrs;
+
+  }
+
   const submitForm = (e) => {
     e.preventDefault();
-    console.log("Form submitted!");
+
+    const validationErrors = validateForm();
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length > 0) {
+      return;
+    }
+
+    console.log("Form submitted!", formData);
   };
 
   return (
@@ -24,13 +66,15 @@ export default function page() {
             <p>Log in to continue using LocalHands.</p>
           </div>
 
-          <form name="login-form" className={styles.loginForm} onSubmit={submitForm}>
+          <form name="login-form" className={styles.loginForm} onSubmit={submitForm} autoComplete="off">
             <label htmlFor="email">Email
-              <input type="email" id="email" name="email" required placeholder="jdoe@gmail.com" autoComplete="email" />
+              <input type="email" id="email" name="email" required placeholder="jdoe@gmail.com" value={formData.email} onChange={handleChange} />
+              {errors.email && <span className={styles.error}>{errors.email}</span>}
             </label>
 
             <label htmlFor="password">Password
-              <input type="password" id="password" name="password" required placeholder="Enter your password" autoComplete="current-password" />
+              <input type="password" id="password" name="password" required placeholder="Enter your password" value={formData.password} onChange={handleChange}/>
+              {errors.password && <span className={styles.error}>{errors.password}</span>}
             </label>
 
             <div className={styles.optionsRow}>
